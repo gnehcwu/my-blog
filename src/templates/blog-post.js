1a5d8aa24a1672a221d5bf6bc85cc11c4b59efaf
Cheng Wu <iwucheng@outlook.com>
2019-01-15 10:44:51 +0000

    add own disqus component

--- src/templates/blog-post.js
+++ src/templates/blog-post.js
@@ -5,7 +5,8 @@ import Bio from '../components/Bio'
 import Layout from '../components/Layout'
 import SEO from '../components/seo'
 import { rhythm, scale } from '../utils/typography'
-import { DiscussionEmbed } from "disqus-react"
+import { DiscussionEmbed } from 'disqus-react'
+import { Disqus } from '../components/disqus'
 
 class BlogPostTemplate extends React.Component {
   render() {
@@ -64,7 +65,8 @@ class BlogPostTemplate extends React.Component {
             )}
           </li>
         </ul>
-        <DiscussionEmbed shortname={disqusShortname} config={disqusConfig} />
+        {/* <DiscussionEmbed shortname={disqusShortname} config={disqusConfig} /> */}
+        <Disqus shortName={disqusShortname} config={disqusConfig} />
       </Layout>
     )
   }
